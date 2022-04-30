b14aa0c629ee7c40469dccbd8d262f15b47380ef
borisevtimov <boriss.evtimov@gmail.com>
2022-05-01 00:26:00 +0300

    Fixed issues with WPFHello

--- WPFhello/MainWindow.xaml.cs
+++ WPFhello/MainWindow.xaml.cs
@@ -22,271 +22,35 @@ namespace WPFhello
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Student? Student { get; set; }
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnClear_Click(object sender, RoutedEventArgs e)
+        private void btn_Hello_Click(object sender, RoutedEventArgs e)
         {
-            ClearForms();
-        }
+            string content = string.Empty;
 
-        private void btnPopulateModel_Click(object sender, RoutedEventArgs e)
-        {
-            PopulateForm();
-        }
-
-        private void btnDeactivateControls_Click(object sender, RoutedEventArgs e)
-        {
-            DeactivateControls();
-        }
-
-        private void btnActivateControls_Click(object sender, RoutedEventArgs e)
-        {
-            ActivateControls();
-        }
-
-        private void btnEnterUser_Click(object sender, RoutedEventArgs e)
-        {
-            EnterUser();
-        }
-
-        private void ClearForms() 
-        {
-            foreach (var item in PersonalData.Children)
+            foreach (var item in MainGrid.Children)
             {
                 if (item is TextBox)
                 {
-                    ((TextBox)item).Text = string.Empty;
-                }
-            }
-
-            foreach (var item in StudentInformation.Children)
-            {
-                if (item is TextBox)
-                {
-                    ((TextBox)item).Text = string.Empty;
-                }
-            }
-        }
-
-        private void PopulateForm() 
-        {
-            StudentData studentData = new StudentData();
-            Student = studentData.TestStudents?.FirstOrDefault();
-
-            foreach (var item in PersonalData.Children)
-            {
-                if (item is TextBox)
-                {
-                    if (((TextBox)item).Name.Equals("txtFirstName"))
+                    if (((TextBox)item).Text.Length < 2)
                     {
-                        ((TextBox)item).Text = Student?.FirstName;
+                        MessageBox.Show("Полето трябва да съдържа поне 2 символа");
                     }
-                    else if (((TextBox)item).Name.Equals("txtMiddleName"))
+                    else
                     {
-                        ((TextBox)item).Text = Student?.MiddleName;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtLastName"))
-                    {
-                        ((TextBox)item).Text = Student?.LastName;
-                    }
-                }
-            }
-
-            foreach (var item in StudentInformation.Children)
-            {
-                if (item is TextBox)
-                {
-                    if (((TextBox)item).Name.Equals("txtFaculty"))
-                    {
-                        ((TextBox)item).Text = Student?.Faculty;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtSpeciality"))
-                    {
-                        ((TextBox)item).Text = Student?.Speciality;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtDegree"))
-                    {
-                        ((TextBox)item).Text = Student?.Degree.ToString();
-                    }
-                    else if (((TextBox)item).Name.Equals("txtFacultyNumber"))
-                    {
-                        ((TextBox)item).Text = Student?.FacultyNumber;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtStatus"))
-                    {
-                        ((TextBox)item).Text = Student?.Status.ToString();
-                    }
-                    else if (((TextBox)item).Name.Equals("txtCourse"))
-                    {
-                        ((TextBox)item).Text = Student?.Course.ToString();
-                    }
-                    else if (((TextBox)item).Name.Equals("txtStream"))
-                    {
-                        ((TextBox)item).Text = Student?.Stream.ToString();
-                    }
-                    else if (((TextBox)item).Name.Equals("txtGroup"))
-                    {
-                        ((TextBox)item).Text = Student?.Group.ToString();
+                        MessageBox.Show("Здрасти " + ((TextBox)item).Text + "!!! \nТова е твоята първа програма на Visual Studio 2022!");
                     }
                 }
             }
         }
 
-        private void DeactivateControls() 
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var item in PersonalData.Children)
-            {
-                if (item is TextBox)
-                {
-                    ((TextBox)item).IsEnabled = false;
-                }
-            }
-
-            foreach (var item in StudentInformation.Children)
-            {
-                if (item is TextBox)
-                {
-                    ((TextBox)item).IsEnabled = false;
-                }
-            }
-        }
-
-        private void ActivateControls() 
-        {
-            foreach (var item in PersonalData.Children)
-            {
-                if (item is TextBox)
-                {
-                    ((TextBox)item).IsEnabled = true;
-                }
-            }
-
-            foreach (var item in StudentInformation.Children)
-            {
-                if (item is TextBox)
-                {
-                    ((TextBox)item).IsEnabled = true;
-                }
-            }
-        }
-
-        private void EnterUser() 
-        {
-            StudentData studentData = new StudentData();
-            Student = studentData.TestStudents?.FirstOrDefault();
-
-            foreach (var item in PersonalData.Children)
-            {
-                if (item is TextBox)
-                {
-                    if (((TextBox)item).Name.Equals("txtFirstName"))
-                    {
-                        Student.FirstName = ((TextBox)item).Text;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtMiddleName"))
-                    {
-                        Student.MiddleName = ((TextBox)item).Text;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtLastName"))
-                    {
-                        Student.LastName = ((TextBox)item).Text;
-                    }
-                }
-            }
-
-            foreach (var item in StudentInformation.Children)
-            {
-                if (item is TextBox)
-                {
-                    if (((TextBox)item).Name.Equals("txtFaculty"))
-                    {
-                        Student.Faculty = ((TextBox)item).Text;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtSpeciality"))
-                    {
-                        Student.Speciality = ((TextBox)item).Text;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtDegree"))
-                    {
-                        Degree degree;
-
-                        if (Enum.TryParse(((TextBox)item).Text, out degree))
-                        {
-                            Student.Degree = degree;
-                        }
-                        else
-                        {
-                            DeactivateControls();
-                            return;
-                        }
-                    }
-                    else if (((TextBox)item).Name.Equals("txtFacultyNumber"))
-                    {
-                        Student.FacultyNumber = ((TextBox)item).Text;
-                    }
-                    else if (((TextBox)item).Name.Equals("txtStatus"))
-                    {
-                        EducationStatus status;
-
-                        if (Enum.TryParse(((TextBox)item).Text, out status))
-                        {
-                            Student.Status = status;
-                        }
-                        else
-                        {
-                            DeactivateControls();
-                            return;
-                        }
-                    }
-                    else if (((TextBox)item).Name.Equals("txtCourse"))
-                    {
-                        int result;
-
-                        if (int.TryParse(((TextBox)item).Text, out result))
-                        {
-                            Student.Course = result;
-                        }
-                        else
-                        {
-                            DeactivateControls();
-                            return;
-                        }
-                    }
-                    else if (((TextBox)item).Text.Equals("txtStream"))
-                    {
-                        int result;
-
-                        if (int.TryParse(((TextBox)item).Text, out result))
-                        {
-                            Student.Stream = result;
-                        }
-                        else
-                        {
-                            DeactivateControls();
-                            return;
-                        }
-                    }
-                    else if (((TextBox)item).Text.Equals("txtGroup"))
-                    {
-                        int result;
-
-                        if (int.TryParse(((TextBox)item).Text, out result))
-                        {
-                            Student.Group = result;
-                        }
-                        else
-                        {
-                            DeactivateControls();
-                            return;
-                        }
-                    }
-                }
-            }
+            MessageBox.Show("This is Windows Presentation Foundation!");
+            textBlock1.Text = DateTime.Now.ToString();
         }
     }
 }
